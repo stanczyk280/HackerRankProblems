9688c9fbbb3991c3bf9b83d6fed2606a8cfe30fe
Bartosz Grabowski <bartek.grabowski.280@gmail.com>
2024-02-26 22:26:42 +0100

    +AppleAndOrange

---
+++ ProblemSolvingEasy/AppleAndOrange.cs
@@ -0,0 +1,25 @@
+﻿namespace ProblemSolvingEasy
+{
+    //https://www.hackerrank.com/challenges/apple-and-orange/problem
+    public static class AppleAndOrange
+    {
+        public static void CountApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+        {
+            int applesLanded = 0;
+            int orangesLanded = 0;
+
+            foreach (int apple in apples)
+            {
+                if (a + apple >= s && a + apple <= t) applesLanded++;
+            }
+
+            foreach (int orange in oranges)
+            {
+                if (b + orange >= s && b + orange <= t) orangesLanded++;
+            }
+
+            Console.WriteLine(applesLanded);
+            Console.WriteLine(orangesLanded);
+        }
+    }
+}
