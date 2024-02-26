819d5282597804c6b98181c6c3bf7a110e74cd44
Bartosz Grabowski <bartek.grabowski.280@gmail.com>
2024-02-26 21:58:33 +0100

    +Grading students, removed not needed files

---
+++ ProblemSolvingEasy/GradingStudents.cs
@@ -0,0 +1,28 @@
+﻿namespace ProblemSolvingEasy
+{
+    public static class GradingStudents
+    {
+        public static List<int> GradeStudents(List<int> grades)
+        {
+            double nearestVal = 0;
+            int convertedNearestVal = 0;
+            for (int i = 0; i < grades.Count; i++)
+            {
+                if (grades[i] < 38)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (grades[i] % 5 >= 3)
+                    {
+                        nearestVal = 5 * Math.Round((double)grades[i] / 5.0);
+                        convertedNearestVal = Convert.ToInt32(nearestVal);
+                        grades[i] = convertedNearestVal;
+                    }
+                }
+            }
+            return grades;
+        }
+    }
+}
